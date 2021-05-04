8a3befc0f8c17d90753d53d2d4622da35fca3b13
bominkm <68378332+bominkm@users.noreply.github.com>
2021-05-04 12:03:51 +0900

    Create exercise01.sol

---
+++ exercise01.sol
@@ -0,0 +1,23 @@
+pragma solidity ^0.4.25;
+
+contract SimpleGrade{
+
+  string student;
+  
+  function setGrade(unit256 score) public{
+    
+    if(score >= 90) {
+      student = 'A';
+    } else if(score >= 80) {
+      student = 'B';
+    } else if(score >= 70) {
+      student = 'C';
+    } else {
+      student = 'F';
+    }
+   }
+   
+   function getGrade() public constant returns (string) {
+      return student;
+   }
+}
