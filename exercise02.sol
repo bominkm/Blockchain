e2957a5de013e9b3293d6fa0a8705a0c64379591
bominkm <68378332+bominkm@users.noreply.github.com>
2021-05-18 12:08:48 +0900

    Update exercise02.sol

--- exercise02.sol
+++ exercise02.sol
@@ -6,12 +6,13 @@ contract classA{
     string public stu_Name;
     string public stu_Dept;
     
-    function setClass (uint _stu_ID, string _stu_Name, string stu_Dept) public{
+    function setClass (uint _stu_ID, string _stu_Name, string _stu_Dept) public {
         stu_ID = _stu_ID;
         stu_Name = _stu_Name;
-        stu_Dept = _stu_Dept
+        stu_Dept = _stu_Dept;
     }
     
     function getClass() public view returns(uint, string, string){
         return(stu_ID, stu_Name, stu_Dept);
     }
+}
