fc93547e32163b883852441ced75a13b4646981e
bominkm <68378332+bominkm@users.noreply.github.com>
2021-06-02 16:33:34 +0900

    Create exercise03.sol

---
+++ exercise03.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.7.6;
+
+contract Count {
+    uint public countnum;
+    
+    function get() public view returns (uint){
+        return countnum;
+    }
+    
+    function inc() public {
+        countnum += 1;
+    }
+    
+    function dec() public {
+        countnum -= 1;
+    }
+}
